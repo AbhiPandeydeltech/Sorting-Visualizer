c2ff9ed288d9161bb5c38158d87096e55171869f
AbhiPandeydeltech <abhishekkman77@gmail.com>
2024-09-09 21:19:36 +0530

    first commit

---
+++ scripts/menu.js
@@ -0,0 +1,30 @@
+
+
+
+/*
+    *****************
+    DONE BY:-   ABHISHEK PANDEY
+    
+    *****************
+*/
+
+let dropdownContent = document.querySelector('.dropdown-content');
+document.querySelector('.dropdown-button').addEventListener('click', function() {
+    
+    
+    // Toggle the display of the dropdown content
+    if (dropdownContent.style.display === "none" ) {
+      dropdownContent.style.display = "block";
+    } else {
+      dropdownContent.style.display = "none";
+    }
+  });
+  
+
+  
+/*
+    *****************
+    DONE BY:-   ABHISHEK PANDEY
+    
+    *****************
+*/
